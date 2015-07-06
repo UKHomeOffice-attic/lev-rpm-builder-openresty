eb7e0f6babe307f65a8233025eadd734b9fd5250
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-07-06 15:54:35 +0100

    Added build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ NAME="rpm-builder-waf"
 TAG="${NAME}:${BUILD_NUMBER:-1}"
 CONTAINER_NAME="${NAME}-build-${BUILD_NUMBER:-1}"
 
-docker build -t $TAG . && \
-docker run \
+sudo docker build -t $TAG . && \
+sudo docker run \
     --name=$NAME \
     -e "RPM_OUTPUT_DIR=/rpms" \
     -v "$(pwd):/rpms" \
     $TAG && \
-docker rm $NAME
+sudo docker rm $NAME
