78a4950155eb2076cca88df9c98cd3d5c39bde41
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-07-08 13:14:43 +0100

    Updated build script with optional sudo

--- build.sh
+++ build.sh
@@ -4,24 +4,33 @@ NAME="rpm-builder-waf"
 TAG="${NAME}:${BUILD_NUMBER:-1}"
 CONTAINER_NAME="${NAME}-build-${BUILD_NUMBER:-1}"
 
-
 TEMP_EXTENSION=tmp-$CONTAINER_NAME-$(date +"%s")
-SHARED_TEMP_HOST=$SHARED_JENKINS_PATH_HOST/$TEMP_EXTENSION
-SHARED_TEMP_CONTAINER=$SHARED_JENKINS_PATH_CONTAINER/$TEMP_EXTENSION
-
-SHARED_JENKINS_PATH_HOST
+SHARED_TEMP_HOST=${SHARED_JENKINS_PATH_HOST:-$(pwd)}/$TEMP_EXTENSION
+SHARED_TEMP_CONTAINER=${SHARED_JENKINS_PATH_CONTAINER:-$(pwd)}/$TEMP_EXTENSION
 
 if [ ! -f $SHARED_TEMP_CONTAINER ]; then
-    mkdir -p $SHARED_TEMP_CONTAINER
+    mkdir -p "${SHARED_TEMP_CONTAINER}"
 fi
 
-sudo docker build -t $TAG . && \
-sudo docker run \
-    --name=$CONTAINER_NAME \
-    -e "RPM_OUTPUT_DIR=/rpms" \
-    -v "$SHARED_TEMP_HOST:/rpms" \
-    $TAG && \
-sudo docker rm $CONTAINER_NAME
+if [ ${SUDO:-1} -eq "1" ]; then
+    sudo docker rm $CONTAINER_NAME ;
+    sudo docker build -t $TAG . && \
+    sudo docker run \
+        --name=$CONTAINER_NAME \
+        -e "RPM_OUTPUT_DIR=/rpms" \
+        -v "$SHARED_TEMP_HOST:/rpms" \
+        $TAG && \
+    sudo docker rm $CONTAINER_NAME
+else
+    docker rm $CONTAINER_NAME ;
+    docker build -t $TAG . && \
+    docker run \
+        --name=$CONTAINER_NAME \
+        -e "RPM_OUTPUT_DIR=/rpms" \
+        -v "$SHARED_TEMP_HOST:/rpms" \
+        $TAG && \
+    docker rm $CONTAINER_NAME
+fi
 
 cp $SHARED_TEMP_CONTAINER/* $(pwd)
 rm -rf $SHARED_TEMP_CONTAINER
